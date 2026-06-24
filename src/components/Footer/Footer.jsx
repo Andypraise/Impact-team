30e4583d6a2eb303faa03779273c459496cf66e0
Andy <andypraise890@gmail.com>
2026-06-24 14:39:18 +0100

    made changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,14 @@
-
-
 function Footer() {
   return (
     <div>
-        <footer className="w-full text-center py-4  min-h-screen bg-linear-to-b lg:bg-linear-to-r from-white via-blue-40 to-blue-600">
-  <p className="text-[17px] text-black">
-    © {new Date().getFullYear()} Aidlink. All Rights Reserved.
-  </p>
-</footer>
+      <footer className="w-full text-center py-4   bg-linear-to-b lg:bg-linear-to-r from-white via-blue-40 to-blue-600">
+        <p className="text-[17px] text-black">
+          © {new Date().getFullYear()} Aidlink. All Rights Reserved.
+        </p>
+        
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
